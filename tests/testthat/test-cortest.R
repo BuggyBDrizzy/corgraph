43b6b96f8dc02a04e8db154d78191ac6780428f7
Bryan Driscoll <driscoll6@gmail.com>
2020-11-18 22:36:04 -0800

    10:35 pm

--- tests/testthat/test-cortest.R
+++ tests/testthat/test-cortest.R
@@ -1,3 +1,22 @@
+
+
+# THESE SHOULD WORK BUT FOR SOME REASON AREN'T   :(
+
+# They check that you
+# a) are passing a valid tibble or data frame
+# b) are entering a character string for the var2 variable
+# c) are entering a character for cormethod variable
+# d) are entering only TRUE or FALSE for the interval variable
+
+# expect_equal(TRUE, is_tibble(datasource)) || expect_equal(TRUE, is.data.frame(datasource))
+# expect_equal(TRUE, is_character(var2))
+# expect_type(cormethod, "character")
+# expect_type(interval, "logical")
+
+# So, the only tests I could include were ones that check the default dataframe object
+# included in the package. These ensure you have the right column names
+
+
 expect_true("CO2Flux" %in% names(Date))
 expect_true("Ca" %in% names(Date))
 expect_true("Mg" %in% names(Date))
